07f3a52017e32807d940b2c6258c9c300586ef1f
Matheus <matheusfontenele979700@gmail.com>
2023-08-03 19:44:14 -0300

    fontenele

---
+++ src/components/Banner/index.js
@@ -0,0 +1,51 @@
+import styles from './Banner.module.css'
+import elipse from '../../assets/imgs/circulo.png'
+import { BiLogoHtml5, BiLogoCss3, BiLogoJavascript, BiLogoTypescript, BiLogoReact, BiLogoSass} from 'react-icons/bi'
+const minhaFoto = 'https://github.com/matheFontenele.png'
+
+
+export default function Banner(){
+    return(
+        <main className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Matheus
+                </h1>
+                <h1 className={styles.titulo}>
+                    Fontenele
+                </h1>
+
+                <p className={styles.paragrafo}>
+                    Desenvolvedor Front End apaixonado por designer web, com conhecimento em HTML5, CSS3, JavaScript e TypesScrip, sempre buscando expandir meus conhecimentos no Front End e em breve começar no mundo do Back End.
+                </p>
+
+
+                <ul className={styles.skills}>
+                    <BiLogoHtml5/>
+                    <BiLogoCss3/>
+                    <BiLogoJavascript/>
+                    <BiLogoTypescript/>
+                    <BiLogoReact/>
+                    <BiLogoSass/>
+                </ul>
+
+                <h2 className={styles.projectsIntro}><i className="fa-solid fa-angle-left"></i>Confira abaixo alguns dos meus principais projetos<i>/</i><i className="fa-solid fa-angle-right"></i></h2>
+
+            </div>
+
+            <div className={styles.imagens}>
+                <img 
+                    className={styles.circuloColorido} 
+                    src={elipse} 
+                    alt='circulo' 
+                    aria-hidden={true}
+                />
+
+                <img 
+                    className={styles.minhaFoto}
+                    src={minhaFoto}
+                    alt='Eu'/>
+            </div>
+        </main>
+    )
+}
